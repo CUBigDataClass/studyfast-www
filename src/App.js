6e97a16e27a5b4616e5d27686d1cee453add3111
Jack Marty <>
2019-04-07 12:52:12 -0600

    Basic Frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Search from './components/Search';
+import Header from './components/layout/header';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: []
+  }
+
+
+  search = () => {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+      .then(res => this.setState({ todos: res.data }))
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <Search search={this.search}/>
+              </React.Fragment>
+            )} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
